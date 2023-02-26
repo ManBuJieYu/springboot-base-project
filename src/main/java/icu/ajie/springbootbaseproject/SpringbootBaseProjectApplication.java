99c4798b66d9b8f0fe007935272e33eb67a9f084
HaoJie <ajie2326@163.com>
2023-02-26 13:35:05 +0800

    2023.02.26 项目初始化

---
+++ src/main/java/icu/ajie/springbootbaseproject/SpringbootBaseProjectApplication.java
@@ -0,0 +1,13 @@
+package icu.ajie.springbootbaseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBaseProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBaseProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/icu/ajie/springbootbaseproject/SpringbootBaseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.ajie.springbootbaseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBaseProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
