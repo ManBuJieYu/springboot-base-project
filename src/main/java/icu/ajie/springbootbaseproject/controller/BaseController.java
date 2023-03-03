3747ce7712d7fa18e9f5f69151a338928697c197
HaoJie <ajie2326@163.com>
2023-03-03 18:20:13 +0800

    初始化

---
+++ src/main/java/icu/ajie/springbootbaseproject/controller/BaseController.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2018-2023 feihe.com All right reserved.
+ * This software is the confidential and proprietary information of feihe.com ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only
+ * in accordance with the terms of the license agreement you entered into with feihe.com.
+ */
+package icu.ajie.springbootbaseproject.controller;
+
+import javax.annotation.Resource;
+
+/**
+ * Controller 模板
+ *
+ * @author HaoJie
+ * @since 2023-03-03
+ */
+
+public class BaseController {
+
+}
